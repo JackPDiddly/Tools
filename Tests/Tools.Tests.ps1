f040757fea6894c8c1ab3b8b4cd6cdae79108d51
JackPDiddly <karrmann@engineer.com>
2023-04-10 14:06:50 -0500

    Update tests to remove Unicode character issues

--- Tests/Tools.Tests.ps1
+++ Tests/Tools.Tests.ps1
@@ -14,9 +14,23 @@ $global:Funtions =  (
 
 
 Describe "Module $Module" {
+
     Context "Test Module $Module" -Tags @("Module","Unit") {
         It "$Module is valid PowerShell code" {
             $global:path = $here + "\" + $Module + ".psm1"
+            $psFile = Get-Content -Path $path -ErrorAction SilentlyContinue
+            $errors = $null         #Creates variable so it is ready for next line
+            if ( $null -eq $psFile ) {
+                $errors = new-object psobject
+                Add-Member -InputObject $errors -MemberType NoteProperty -Name "Count" -Value 1
+            } else {
+                $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            } #If
+            $errors.Count | Should -Be 0
+        } # It
+
+        It "$Module has a manifest" {
+            $global:path = $here + "\" + $Module + ".psd1"
             $psFile = Get-Content -Path $path -ErrorAction SilentlyContinue
             $errors = $null         #Creates variable so it is ready for next line
             if ( $null -eq $psFile ) {

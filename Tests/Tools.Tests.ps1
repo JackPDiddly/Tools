cf2b635b22cdbc6491381815f7158d12dcf1b9be
JackPDiddly <karrmann@engineer.com>
2023-04-12 12:56:57 -0500

    adding module manifest test

--- Tests/Tools.Tests.ps1
+++ Tests/Tools.Tests.ps1
@@ -29,7 +29,7 @@ Describe "Module $Module" {
             $errors.Count | Should -Be 0
         } # It
 
-        It "$Module has a manifest" {
+        It "$Module has a manifest which is valid PowerShell code" {
             $global:path = $here + "\" + $Module + ".psd1"
             $psFile = Get-Content -Path $path -ErrorAction SilentlyContinue
             $errors = $null         #Creates variable so it is ready for next line
@@ -41,5 +41,32 @@ Describe "Module $Module" {
             } #If
             $errors.Count | Should -Be 0
         } # It
+
+        It "$Module manifest has the proper GUID" {
+            $global:path = $here + "\" + $Module + ".psd1"
+            $psFile = Get-Content -Path $path -ErrorAction SilentlyContinue
+            if ( $null -eq $psFile ) {
+                $results = $false
+            } else {
+                for ( ($results=$false), ($i=0) ; $i -lt $psFile.Count ; $i++ ) {
+                    $Result = [System.Guid]::empty #Reference for the output, required by the method but not useful in powershell
+                    if ( $psFile[$i] -like "GUID = *" ) {
+                        if ([System.Guid]::TryParse("34823b02-4e79-41ff-908b-3e32fe36e1ef", [System.Management.Automation.PSReference]$Result)) {  # Returns true if successfully parsed, otherwise false.
+                            $results = $true
+                            break
+                        }
+                    }
+                }
+            } #If
+            $results| Should -Be $true
+        } # It
     } # Context
 } # Describe
+
+
+
+$Result = [System.Guid]::empty #Reference for the output, required by the method but not useful in powershell
+[System.Guid]::TryParse("foo",[System.Management.Automation.PSReference]$Result) # Returns true if successfully parsed and assigns the parsed guid to $Result, otherwise false.
+
+$Result = [System.Guid]::empty #Reference for the output, required by the method but not useful in powershell
+[System.Guid]::TryParse("12345678-1234-1234-1234-987654321abc",[System.Management.Automation.PSReference]$Result) # Returns true if successfully parsed, otherwise false.
